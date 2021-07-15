0d3288699caa407cc4b9853c2f79dd453a31b338
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2021-07-15 20:11:29 +0530

    update

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -8,8 +8,7 @@ import javafx.scene.control.Button;
 
 import javax.swing.*;
 
-public class Controller
-{
+public class Controller {
     public TextField username;
     public RadioButton novice;
     public RadioButton intermediate;
@@ -20,29 +19,23 @@ public class Controller
     private int nDisc;
 
 
-
-
-    public void initialize()
-    {
+    public void initialize() {
         play_btn.setDisable(true);
 
     }
+
     // return number of Disc
-    private int numberOfDisc()
-    {
+    private int numberOfDisc() {
 //        RadioButton selectedRadioButton = (RadioButton) level.getSelectedToggle();
 //        String value = selectedRadioButton.getText();
 //        System.out.println(value);
-        if(this.level.getSelectedToggle().equals(this.expert))
-        {
-            nDisc=9;
+        if (this.level.getSelectedToggle().equals(this.expert)) {
+            nDisc = 9;
         }
-        if(this.level.getSelectedToggle().equals(this.intermediate))
-        {
-            nDisc=6;
-        }
-        else{
-            nDisc=4;
+        if (this.level.getSelectedToggle().equals(this.intermediate)) {
+            nDisc = 6;
+        } else {
+            nDisc = 4;
         }
         return nDisc;
     }
@@ -54,16 +47,16 @@ public class Controller
                     Game.t = new Tower(numberOfDisc());//number of Disks
                     Game.f.getContentPane().add(Game.t);
                 }
-            );
+        );
     }
-    public void keyReleasedProperty(){
+
+    public void keyReleasedProperty() {
         String userName = username.getText();
         boolean isDisabled = (userName.isEmpty() || userName.trim().isEmpty());
         play_btn.setDisable(isDisabled);
     }
 
-    public void solveHandler(ActionEvent event)
-    {
+    public void solveHandler(ActionEvent event) {
         SwingUtilities.invokeLater(() ->
         {
             try {
@@ -84,11 +77,11 @@ public class Controller
             HighScores highScores = new HighScores();
             highScores.view();
             HighscoreManager hm = new HighscoreManager();
-            hm.addScore("Bart",2240);
-            hm.addScore("Marge",300);
-            hm.addScore("Maggie",220);
-            hm.addScore("Homer",100);
-            hm.addScore("Lisa",270);
+            hm.addScore("Bart", 2240);
+            hm.addScore("Marge", 300);
+            hm.addScore("Maggie", 220);
+            hm.addScore("Homer", 100);
+            hm.addScore("Lisa", 270);
 
             System.out.println(username.getText());
 
@@ -96,4 +89,6 @@ public class Controller
         });
 
     }
+}
+
 
